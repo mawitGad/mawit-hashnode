30bca5d9f6e5ff6b8b67fd880914d4f2ca7dd468
mawit <mawitgaddev@gmail.com>
2023-04-07 07:19:03 +0100

    Fix: make top detail's text stay solid by setting baseline befor redering

--- assets/canvasAssets/topDetails.ts
+++ assets/canvasAssets/topDetails.ts
@@ -11,6 +11,7 @@ export class TopDetails {
 
 		ctx.font = "14px sans-serif";
 		ctx.fillStyle = "#95A3B8";
+		ctx.textBaseline = "alphabetic";
 		ctx.fillText("Pageviews", canvasCenterX, 19);
 		ctx.fillRect(canvasCenterX - this.widthPercentile(20) - 10, 10, this.widthPercentile(20), 10);
 	}
